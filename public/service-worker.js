509b8629f203b63322e0fe675f915978d34151a2
Tyler McBride <tymcbrid@gmail.com>
2020-08-07 23:55:54 -0700

    bug fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/index.js',
     '/db.js',
-    '/manifest.webmanifest',
+    '/manifest.json',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
